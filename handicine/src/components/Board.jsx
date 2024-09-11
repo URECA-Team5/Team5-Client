0ab72de679ed1cc64585fb04649f6dbaf3ea16db
SeokBo <kbs9901@gmail.com>
2024-09-11 15:39:45 +0900

    로그인,회원가입 폼 update

--- handicine/src/components/Board.jsx
+++ handicine/src/components/Board.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 import { Dropdown, DropdownButton, FormControl, InputGroup } from 'react-bootstrap';
 import { FaSearch } from 'react-icons/fa'; // 돋보기 아이콘
 
-const QnAPage = () => {
+const Board = () => {
   const [questions, setQuestions] = useState([
     { id: 1, title: "전문가에게 질문하고 싶어요", author: "사용자1", date: "2024-09-01" },
     { id: 2, title: "약물 복용 관련 질문이 있습니다", author: "사용자2", date: "2024-09-02" },
@@ -35,45 +35,46 @@ const QnAPage = () => {
 
   return (
     <div className="qna-page">
-      <h1>전문가 Q&A 게시판</h1>
+      <h1 className='page-title'>자유 게시판</h1>
+      <div className="container-box">
+        <div className="search-bar">
+          <InputGroup className="custom-search">
+            <FormControl
+              placeholder="검색어를 입력해주세요"
+              aria-label="Search"
+              onChange={(e) => setSearchText(e.target.value)}
+            />
+            <InputGroup.Text className="search-icon" onClick={handleSearch}>
+              <FaSearch style={{ color: 'green' }} />
+            </InputGroup.Text>
+          </InputGroup>
+        </div>
 
-      <div className="search-bar">
-        <InputGroup className="custom-search">
-          <DropdownButton
-            variant="outline-secondary"
-            title={searchOption}
-            id="input-group-dropdown"
-            onSelect={handleSearchOptionSelect}
-          >
-            <Dropdown.Item eventKey="제목 + 게시글">제목 + 게시글</Dropdown.Item>
-            <Dropdown.Item eventKey="제목">제목</Dropdown.Item>
-            <Dropdown.Item eventKey="작성자">작성자</Dropdown.Item>
-            <Dropdown.Item eventKey="댓글내용">댓글내용</Dropdown.Item>
-          </DropdownButton>
-          <FormControl
-            placeholder="검색어를 입력해주세요"
-            aria-label="Search"
-            onChange={(e) => setSearchText(e.target.value)}
-          />
-          <InputGroup.Text className="search-icon" onClick={handleSearch}>
-            <FaSearch style={{ color: 'green' }} />
-          </InputGroup.Text>
-        </InputGroup>
-      </div>
-
-      <div className="qna-list">
-        {questions.map(question => (
-          <div key={question.id} className="qna-item">
-            <Link to={`/qna/${question.id}`}>
-              <h2>{question.title}</h2>
-            </Link>
-            <p>작성자: {question.author}</p>
-            <p>작성일: {question.date}</p>
-          </div>
-        ))}
+        <table className="qna-table">
+          <thead>
+            <tr>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {questions.map(question => (
+              <tr key={question.id}>
+                <td>
+                  <Link to={`/board/${question.id}`}>
+                    {question.title}
+                  </Link>
+                </td>
+                <td>{question.author}</td>
+                <td>{question.date}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
       </div>
     </div>
   );
 };
 
-export default QnAPage;
+export default Board;
